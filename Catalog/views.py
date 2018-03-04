23b9b462b810cabe6729bd327c8e5519c49e0a54
hakemkadem <hakemk21@gmail.com>
2018-03-04 11:06:41 +0300

    First version of application moved into github

--- Catalog/views.py
+++ Catalog/views.py
@@ -88,7 +88,7 @@ def renew_book_librarian(request, pk):
             book_inst.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse('all-borrowed'))
 
     # If this is a GET (or any other method) create the default form.
     else:
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -129,3 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/Catalog/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
